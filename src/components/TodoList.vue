79549943e8b862422140d14d26f883c072c0353d
Adriano Oliveira Souza <aosouza@stefanini.com>
2023-02-24 14:22:52 -0300

    Implement todolist render and mark completed

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,8 +1,26 @@
 <script setup>
+import { storeToRefs } from 'pinia';
+import { useTodoListStore } from '../stores/todoList'
+
+const store = useTodoListStore()
+
+const { todoList } = storeToRefs(store)
+
+const { toggleCompleted } = store
+
 </script>
 
 <template>
-    <div></div>
+    <div v-for="todo in todoList" :key="todo.id" class="item">
+        <div class="content">
+            <span :class="{ completed: todo.completed }">{{ todo.item }}</span>
+            <span @click.stop="toggleCompleted(todo.id)">&#10004;</span>
+        </div>
+    </div>
 </template>    
 
-<style scoped></style>
+<style scoped>
+.completed {
+    text-decoration: line-through;
+}
+</style>
--- src/stores/todoList.js
+++ src/stores/todoList.js
@@ -8,6 +8,17 @@ export const useTodoListStore = defineStore('todoList', {
     actions: {
         addTodo(item) {
             this.todoList.push({item, id: this.id++, completed: false})
+        },
+        deleteTodo(itemID) {
+            this.todoList = this.todoList.filter((object) => {
+                return object.id !== itemID
+            })
+        },
+        toggleCompleted(id) {
+            const todo = this.todoList.find((item) => item.id === id)
+            
+            if(todo)
+                todo.completed = !todo.completed
         }
     }
 })
